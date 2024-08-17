0f4cf26ecdd2a5e56aa2de909b98c5d4ff6728df
syed.usman.ali <usmanshahmcom@gmail.com>
2024-08-17 23:36:00 +0500

    image changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-right  p-5">
-      <div/><figure className="bg-white-400 rounded-xl p-8 md:p-0 dark:bg-white-800"/>
+    <main className="flex min-h-screen flex-col items-left p-4">
+<div><figure className="md:flex bg-blue-100 rounded-xl p-8 md:p-0 dark:bg-blue-800">
   <img className="w-24 h-28 square-full mx-auto" src="/usman.jpg" alt="" width="384" height="512"/>
-  <div className="pt-6 space-y-4">
+  <div className="pt-6 md:text-left space-y-4">
     <blockquote>
       <p className="text-lg font-medium">
         “I am Syed Usman Ali from Rahim Yar Khan. I have done M.com(Finance) and M.sc Math and now working as Mathmatics Teacher In Education Department,Rahim Yar Khan.”
@@ -20,7 +20,7 @@ export default function Home() {
       </div>
     </figcaption>
   </div>
-<figure/><div/>
+</figure></div>
 </main>
  );
 }
